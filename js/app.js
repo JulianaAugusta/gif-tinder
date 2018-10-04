c01f2878b8a626dd6f368506f1223bcd8fc7fd35
paclaraujo <paclaraujo@gmail.com>
2018-10-04 18:11:34 -0300

    Firebase database

--- js/app.js
+++ js/app.js
@@ -1,39 +1,37 @@
 $(document).ready(function() {
-  index = 0;
-  showGifs();
-  $('#save-gif').on('click', saveGif);
-});
-
-let index = 0;
-
-var element = document.getElementById('main');
-var hammertime = Hammer(element).on('swipe', function (event) {
- showGifs();
-})
-
-function getGifsOnDb() {
-  const url = 'https://api.giphy.com/v1/gifs/trending?&api_key=JyoICdd17KrRiQnSkR7W8FjphIOQtEDY&limit=50';
-
-  return fetch(url)
-  .then(response => response.json())
-  .then(json => json.data)
-  .catch(error => console.log(error));
-}
-
-async function showGifs() {
-  const data = await getGifsOnDb();
-  const imgUrl = data[index].images.fixed_height.url
-  index++;
-
-  return document.getElementById('main').innerHTML = template(imgUrl);
-}
-
-function template(imgUrl) {    
-  return `
-    <img src="${imgUrl}" id="my-gif">
-  `;
-}
-
-function saveGif() {
-  showGifs();
-}
+    showGifs();
+    $('#save-gif').on('click', saveGif);
+  });
+  
+  var element = document.getElementById('main');
+  var hammertime = Hammer(element).on('swipe', function (event) {
+   showGifs();
+  })
+  
+  function getGifsOnDb() {
+    const url = 'https://api.giphy.com/v1/gifs/random?&api_key=JyoICdd17KrRiQnSkR7W8FjphIOQtEDY&limit=50';
+  
+    return fetch(url)
+    .then(response => response.json())
+    .then(json => json.data)
+    .catch(error => console.log(error));
+  }
+  
+  async function showGifs() {
+    const data = await getGifsOnDb();
+    const imgUrl = data.fixed_height_downsampled_url;
+    console.log(data);
+  
+    return document.getElementById('main').innerHTML = template(imgUrl);
+  }
+  
+  function template(imgUrl) {  
+    return `
+      <img src="${imgUrl}" id="my-gif">
+    `;
+  }
+  
+  function saveGif() {
+    alert('olar');
+    showGifs();
+  }
