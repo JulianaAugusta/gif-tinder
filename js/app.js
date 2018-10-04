611372bb3787433138fe7e3c3b82ee431a00c970
msilva <marcia.silva1802@gmail.com>
2018-10-04 16:57:23 -0300

    gif funcionando

--- js/app.js
+++ js/app.js
@@ -9,13 +9,15 @@ var hammertime = Hammer(element).on('swipe', function (event) {
 })
 
 function getGifsOnDb() {
-  const url = 'https://api.giphy.com/v1/gifs/random?&api_key=pMLsrC3QAvqHYhsBAHn0tJQUqYFzRuZJ&limit=10';
+  const url = 'https://api.giphy.com/v1/gifs/random?&api_key=JyoICdd17KrRiQnSkR7W8FjphIOQtEDY&limit=50';
 
   return fetch(url)
   .then(response => response.json())
   .then(json => json.data)
   .catch(error => console.log(error));
 }
+
+// const 
 
 async function showGifs() {
   const data = await getGifsOnDb();
