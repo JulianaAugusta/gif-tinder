03a50f1665db2a970d5ffec2f0d5df890dd84974
msilva <marcia.silva1802@gmail.com>
2018-10-04 17:38:31 -0300

    gifs funcionando

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  index = 0;
   showGifs();
   $('#save-gif').on('click', saveGif);
 });
+
+let index = 0;
 
 var element = document.getElementById('main');
 var hammertime = Hammer(element).on('swipe', function (event) {
@@ -9,7 +12,7 @@ var hammertime = Hammer(element).on('swipe', function (event) {
 })
 
 function getGifsOnDb() {
-  const url = 'https://api.giphy.com/v1/gifs/random?&api_key=JyoICdd17KrRiQnSkR7W8FjphIOQtEDY&limit=50';
+  const url = 'https://api.giphy.com/v1/gifs/trending?&api_key=JyoICdd17KrRiQnSkR7W8FjphIOQtEDY&limit=50';
 
   return fetch(url)
   .then(response => response.json())
@@ -19,19 +22,18 @@ function getGifsOnDb() {
 
 async function showGifs() {
   const data = await getGifsOnDb();
-  const imgUrl = data.fixed_height_downsampled_url;
-  console.log(data);
+  const imgUrl = data[index].images.fixed_height.url
+  index++;
 
   return document.getElementById('main').innerHTML = template(imgUrl);
 }
 
-function template(imgUrl) {  
+function template(imgUrl) {    
   return `
     <img src="${imgUrl}" id="my-gif">
   `;
 }
 
 function saveGif() {
-  alert('olar');
   showGifs();
 }
